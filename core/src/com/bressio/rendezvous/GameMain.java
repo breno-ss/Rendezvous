7ba0c41be4eafde3d67530cff18560ce9fdf0700
Breno Sousa <bress.io@outlook.com>
2018-05-20 11:48:26 -0300

    Tested GPG key

--- core/src/com/bressio/rendezvous/GameMain.java
+++ core/src/com/bressio/rendezvous/GameMain.java
@@ -14,6 +14,7 @@ public class GameMain extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+        System.out.println("Testing GPG key");
 	}
 
 	@Override
