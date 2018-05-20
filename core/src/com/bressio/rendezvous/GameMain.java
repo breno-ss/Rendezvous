cd6ccd85eeceb57081987d309444d1c505cce82c
Breno Sousa <bress.io@outlook.com>
2018-05-20 11:04:14 -0300

    Testing Git integration with IntelliJ IDEA

--- core/src/com/bressio/rendezvous/GameMain.java
+++ core/src/com/bressio/rendezvous/GameMain.java
@@ -14,6 +14,7 @@ public class GameMain extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		System.out.println("Testing Git integration with IntelliJ IDEA");
 	}
 
 	@Override
